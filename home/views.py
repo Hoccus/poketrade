147b98eb93d33db1dbfd05bb49ee134c0ae2c2a0
Hoc <hocn7379@gmail.com>
2025-04-08 15:34:18 -0400

    Add prototype nav bar and about page

--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
-    template_data = {'title': 'Movies Store'}
+    template_data = {'title': 'PokéTrade'}
     return render(request, 'index.html', {'template_data': template_data})
 def about(request):
     return render(request, 'about.html')
