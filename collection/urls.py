65031949d3cc7405355a8acde3ffd22aca26d611
rxjdasiwzl <2954235311@qq.com>
2025-04-10 22:21:28 -0400

    The collection page with random pokemons

--- collection/urls.py
+++ collection/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index, name='collection.index'),
+    path('', views.get_cards, name='collection.get_cards'),
 ]
