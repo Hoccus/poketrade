f7a243ab0b2f6479f093906623f791667ae9d6e3
rxjdasiwzl <2954235311@qq.com>
2025-04-10 23:16:20 -0400

    put pokemons data into the PokemonCard model (and collection just import data from that)

--- collection/views.py
+++ collection/views.py
@@ -1,25 +1,8 @@
 import requests
 from django.shortcuts import render
 
+from pokemonCard.models import PokemonCard
 
 def get_cards(request):
-    url = "https://pokeapi.co/api/v2/pokemon?limit=20"  # Get first 20 Pokémon
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        raw_data = response.json()["results"]
-
-        # Get name + sprite from individual Pokémon detail endpoint
-        pokemon_list = []
-        for entry in raw_data:
-            poke_resp = requests.get(entry["url"])
-            if poke_resp.status_code == 200:
-                poke_data = poke_resp.json()
-                pokemon_list.append({
-                    "name": poke_data["name"].capitalize(),
-                    "sprite": poke_data["sprites"]["front_default"]  # front_default = normal small sprite
-                })
-    else:
-        pokemon_list = []
-
+    pokemon_list = PokemonCard.objects.all()
     return render(request, 'collection/index.html', {'pokemon_list': pokemon_list})
