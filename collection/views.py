f752fb6617cfd997755d6f58991a0aac4951121b
rxjdasiwzl <2954235311@qq.com>
2025-04-10 21:12:08 -0400

    just started

--- collection/views.py
+++ collection/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from pokemonCard.models import PokemonCard
+import request
 
-# Create your views here.
+def get_card_image(request) :
