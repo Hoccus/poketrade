274ef699d86ed7858418910452bec75ed772ffbf
Hoc Nguyen <74936888+Hoccus@users.noreply.github.com>
2025-04-12 19:46:38 -0400

    Revert "Collection page"

--- collection/views.py
+++ collection/views.py
@@ -1,8 +1,3 @@
-import requests
 from django.shortcuts import render
 
-from pokemonCard.models import PokemonCard
-
-def get_cards(request):
-    pokemon_list = PokemonCard.objects.all()
-    return render(request, 'collection/index.html', {'pokemon_list': pokemon_list})
+# Create your views here.
