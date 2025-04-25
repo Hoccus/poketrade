f9e717569eafc6bb180f96ba4b5eb1b63721b183
Cardin <cardinhschool@gmail.com>
2025-04-25 18:17:19 -0400

    password reset and navbar adjustments

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('logout', views.logout, name='accounts.logout'),
     path('reset_password', auth_views.PasswordResetView.as_view(template_name="accounts/reset_password.html"), name="reset_password"),
     path('orders/', views.orders, name='accounts.orders'),
+    path('reset_password_sent', auth_views.PasswordResetDoneView.as_view(template_name="accounts/reset_password_sent.html"), name="password_reset_done"),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name="accounts/reset_password_confirm.html"), name="password_reset_confirm"),
+    path('reset_password_complete', auth_views.PasswordResetCompleteView.as_view(template_name="accounts/reset_password_complete.html"), name="password_reset_complete"),
 ]
