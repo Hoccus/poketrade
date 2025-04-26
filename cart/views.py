c74c648366304450bba0ff0ee6fc790cb5b8abc1
Cardin <cardinhschool@gmail.com>
2025-04-25 22:28:22 -0400

    made marketplace work, and cart work

--- cart/views.py
+++ cart/views.py
@@ -4,29 +4,51 @@ from marketplace.models import MarketListing
 from .utils import calculate_cart_total
 from .models import Order, CartItem
 from django.contrib.auth.decorators import login_required
+from collection.models import CollectionItem
 
 @login_required
 def purchase(request):
     cart = request.session.get('cart', {})
     listing_ids = list(cart.keys())
-    if (listing_ids == []):
+
+    if not listing_ids:
         return redirect('cart.index')
+
     listings_in_cart = MarketListing.objects.filter(id__in=listing_ids)
     cart_total = calculate_cart_total(cart, listings_in_cart)
+
+    if request.user.userprofile.pokeCoins < cart_total:
+        messages.error(request, "You don't have enough PokeCoins to complete this purchase.")
+        return redirect('cart.index')
+
     order = Order()
     order.user = request.user
     order.total = cart_total
     order.save()
+
     for listing in listings_in_cart:
         item = CartItem()
         item.marketListing = listing
         item.price = listing.price
         item.order = order
         item.save()
+
+        CollectionItem.objects.create(
+            user=request.user,
+            pokemon=listing.item.pokemon,
+            status='collection'
+        )
+
+    request.user.userprofile.pokeCoins -= cart_total
+    request.user.userprofile.save()
+
     request.session['cart'] = {}
-    template_data = {}
-    template_data['title'] = 'Purchase confirmation'
-    template_data['order_id'] = order.id
+
+    template_data = {
+        'title': 'Purchase confirmation',
+        'order_id': order.id
+    }
+
     return render(request, 'cart/purchase.html',{'template_data': template_data})
 
 def index(request):
